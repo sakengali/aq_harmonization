7ff357641f384a35fc615fa67d38a0a189c0187e
Sakengali <sakengali.kazhiyev@nu.edu.kz>
2024-01-19 09:56:37 +0600

    fourth commit

--- AirAssure/unorganized data/csv_combine.py
+++ AirAssure/unorganized data/csv_combine.py
@@ -9,7 +9,6 @@ for folder in folders:
     os.chdir(folder)
     extension = 'csv'
     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-    print(all_filenames)
     #combine all files in the list
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
     #export to csv
